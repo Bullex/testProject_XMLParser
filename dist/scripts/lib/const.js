7e7d980e7b5eec9871fbf429d903798ace3b7a19
Alexander Abumov <abumov.alex@gmail.com>
2016-10-22 20:23:53 +0300

    Add files for GitPages #2

---
+++ dist/scripts/lib/const.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const MESSAGE_XML_URL_IS_EMPTY = 'Путь к XML файлу не найден';
+const MESSAGE_XML_NOT_LOADED = 'XML файл не удалось загрузить';
+const MESSAGE_XML_SUCCESSFULLY_LOADED = 'XML файл успешно загрузился!';
---
+++ dist/scripts/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function initParser() {
+  XML.get(Common.getUrlParameter('xml'));
+}
+
+initParser();
